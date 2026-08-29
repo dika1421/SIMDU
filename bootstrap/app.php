f97184bd0729af8fcce2c221bb1a1f75e765fc6b
dika1421 <muhamaddikap17@gmail.com>
2026-08-29 11:23:10 +0700

    Tambahkan alias middleware permission ke bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'check.role' => \App\Http\Middleware\CheckLoginRole::class,
+            'permission' => \App\Http\Middleware\CheckPermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
